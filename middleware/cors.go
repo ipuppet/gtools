2e3436d0708e49bb83576e5ff925784058026ade
Samuel <zimingw.ol@outlook.com>
2023-12-14 18:14:00 +0800

    extend "Access-Control-Allow-Methods"

--- middleware/cors.go
+++ middleware/cors.go
@@ -31,7 +31,7 @@ func Cors(corsConfig *config.Config, app string) gin.HandlerFunc {
 		for _, allowHost := range cors.([]interface{}) {
 			if origin == allowHost.(string) {
 				c.Header("Access-Control-Allow-Origin", origin)
-				c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+				c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH, TRACE, CONNECT, HEAD")
 				c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 				c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 				c.Header("Access-Control-Allow-Credentials", "true")
